51e51f20c64f9c4fc69b1920d4088cf1fed4131f
Javier <jsg8405@gmail.com>
2018-11-16 06:11:22 +0100

    update server

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 //Express HBS Engine
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
-
-
 
 app.get('/', (req, res) => {
     res.render('home', {
@@ -23,14 +21,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 });
-
-
-app.get('/Data', (req, res) => {
-
-
-})
-
-
 
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${port}`);
